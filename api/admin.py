2652a5ee3363ce9fca75b86b7976a07a622f1e5f
Benjamin Idewor <benjaminidewor@gmail.com>
2021-08-29 02:08:50 +0100

    add models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Movie, Director
+
+admin.site.register(Movie)
+admin.site.register(Director)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
+from turtle import title
+import uuid
 from django.db import models
 
-# Create your models here.
+class Director(models.Model):
+  id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+
+  def __str__(self):
+    return f'{self.first_name} {self.last_name}'
+
+
+class Movie(models.Model):
+  id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+  title = models.CharField(max_length=200)
+  year = models.IntegerField(default=2000)
+  director = models.ForeignKey(Director, on_delete=models.PROTECT, blank=True, null=True)
+
+  def __str__(self):
+    return self.title
+
